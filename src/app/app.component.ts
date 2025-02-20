ccc597d86de37a0161b4742aa453c6a02614d83a
ypeain <yoonpyae26@gmail.com>
2025-02-20 10:51:47 +0630

    Update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuModule, BadgeModule, RippleModule, AvatarModule],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'angular-pj';
-  a: string = '1';
-  b: number = 0;
+  items: MenuItem[] | undefined;
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.b = this.getvalue();
-    }, 1000);
-  }
-
-  getvalue(): number {
-    return 100;
+  ngOnInit() {
+    this.items = [
+      {
+        separator: true,
+      },
+      {
+        label: 'Documents',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+            shortcut: '⌘+N',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+            shortcut: '⌘+S',
+          },
+        ],
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+            shortcut: '⌘+O',
+          },
+          {
+            label: 'Messages',
+            icon: 'pi pi-inbox',
+            badge: '2',
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            shortcut: '⌘+Q',
+          },
+        ],
+      },
+      {
+        separator: true,
+      },
+    ];
   }
 }
